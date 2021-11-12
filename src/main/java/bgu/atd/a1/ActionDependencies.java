ae7ccd787a53477213c01e05c0f4978a7788f69e
Ido <idorev@post.bgu.ac.il>
2021-11-12 17:49:40 +0200

    ActionDependencies class was added - data structure for managing action dependencies

--- src/main/java/bgu/atd/a1/ActionDependencies.java
+++ src/main/java/bgu/atd/a1/ActionDependencies.java
@@ -2,20 +2,21 @@ package bgu.atd.a1;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 
 public class ActionDependencies {
     /**
      * ActionDependencies - an Action dependencies data structure
      * dependenciesCollection - the actual dependencies' container
-     * remainingActionCollection - non-resolved action names list
+     * remainingActionCollection - <ActionName, Action> non-resolved action names container
      * allIsResolved - implies which all the dependencies are resolved
      *
      */
 
-    private final Collection<Action<?>> dependenciesCollection;
+    private final Collection<Action<?>> dependenciesCollection; // used to store and quick access to the dependencies actions results
     //    private final Collection<? extends Action> dependenciesCollection; //TODO: check the use of <? extends Action<?>> - empty constructor and add dependency problem related
-    private final Collection<String> remainingActionsCollection;
+    private final HashMap<String, Action<?>> remainingActionsCollection; // used to control in remaining dependencies
     private boolean allIsResolved;
 
     /**
@@ -23,7 +24,7 @@ public class ActionDependencies {
      */
     public ActionDependencies() {
         this.dependenciesCollection = new ArrayList<Action<?>>();
-        this.remainingActionsCollection = new ArrayList<String>();
+        this.remainingActionsCollection = new HashMap<String, Action<?>>();
         resolvedAllCheck();
     }
 
@@ -32,9 +33,9 @@ public class ActionDependencies {
      */
     public ActionDependencies(Collection<Action<?>> dependenciesCollection) {
         this.dependenciesCollection = dependenciesCollection;
-        this.remainingActionsCollection = new ArrayList<String>();
+        this.remainingActionsCollection = new HashMap<String, Action<?>>();
         for (Action<?> action : dependenciesCollection){
-            this.remainingActionsCollection.add(action.getActionName());
+            this.remainingActionsCollection.put(action.getActionName(),action);
         }
         resolvedAllCheck();
     }
@@ -56,7 +57,7 @@ public class ActionDependencies {
     public boolean addDependency(Action<?> newDependency){
         boolean additionSuccess = this.dependenciesCollection.add(newDependency);
         if(additionSuccess && !newDependency.getResult().isResolved()) {
-            this.remainingActionsCollection.add(newDependency.getActionName());
+            this.remainingActionsCollection.put(newDependency.getActionName(),newDependency);
             resolvedAllCheck();
         }
         return additionSuccess;
@@ -66,20 +67,20 @@ public class ActionDependencies {
         return dependenciesCollection;
     }
 
-    public boolean getAllIsResolved(){
-        return allIsResolved;
-    }
-
     /**
-     * upon dependency satisfaction remove the dependency from the remaining dependencies list
-     * @param action - the satisfied dependency
+     * the method includes the updating stage of the overall isAllResolved value.
+     * @return - return bool value of all the dependencies are resolved.
      */
-    public void removeResolvedDependency(Action<?> action){
-        if(!action.getResult().isResolved())
-            throw new IllegalStateException(" Action promise is not resolved, action name: "+action.getActionName());
-        boolean isRemoved = this.remainingActionsCollection.remove(action.getActionName());
-        if(isRemoved)
+    public boolean isAllResolved(){ // changed cause of flow constrains
+        if(!this.allIsResolved){
+            for (String actionName : this.remainingActionsCollection.keySet()){
+                if(this.remainingActionsCollection.get(actionName).getResult().isResolved()){
+                    this.remainingActionsCollection.remove(actionName);
+                }
+            }
             resolvedAllCheck();
+        }
+        return this.allIsResolved;
     }
 
     /**
