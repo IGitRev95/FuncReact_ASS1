a9ac56a21c0290fde56b7385eb94a97c37cc94b1
Ido <idorev@post.bgu.ac.il>
2021-11-25 17:08:25 +0200

    OpenNewPlacesInACourseAction - implemented

---
+++ src/main/java/bgu/atd/a1/sim/actions/OpenNewPlacesInACourseAction.java
@@ -0,0 +1,27 @@
+package bgu.atd.a1.sim.actions;
+
+import bgu.atd.a1.Action;
+import bgu.atd.a1.sim.privateStates.CoursePrivateState;
+
+/**
+ *  Increases the number of available spaces for the given course.
+ *  Should be initially submitted to the course's actor.
+ */
+public class OpenNewPlacesInACourseAction extends Action<Boolean> {
+    private final Integer extensionAmount;
+
+    public OpenNewPlacesInACourseAction(Integer extensionAmount) {
+        this.setActionName("OpenNewPlacesInACourseAction");
+        this.extensionAmount = extensionAmount;
+    }
+
+    @Override
+    protected void start() {
+        CoursePrivateState coursePS = ((CoursePrivateState)this.actorState);
+        int availableSpots = coursePS.getAvailableSpots();
+        if(availableSpots>-1 && this.extensionAmount>=0){ // Course is open check && legal amount
+            coursePS.setAvailableSpots(availableSpots+this.extensionAmount);
+            this.complete(true);
+        }else{this.complete(false);}
+    }
+}
