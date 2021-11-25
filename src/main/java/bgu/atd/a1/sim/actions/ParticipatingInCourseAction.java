3253e674283287d5c9ce0e0db8abf6b655dfc30c
Ido <idorev@post.bgu.ac.il>
2021-11-25 17:38:24 +0200

    ParticipatingInCourseAction - skeleton

---
+++ src/main/java/bgu/atd/a1/sim/actions/ParticipatingInCourseAction.java
@@ -0,0 +1,23 @@
+package bgu.atd.a1.sim.actions;
+
+import bgu.atd.a1.Action;
+
+public class ParticipatingInCourseAction extends Action<Boolean> {
+    private final String studentName;
+    private final Integer studentGrade;
+
+    public ParticipatingInCourseAction(String studentName) {
+        this.studentName = studentName;
+        this.studentGrade = null;
+    }
+
+    public ParticipatingInCourseAction(String studentName, Integer studentGrade) {
+        this.studentName = studentName;
+        this.studentGrade = studentGrade;
+    }
+
+    @Override
+    protected void start() {
+
+    }
+}
