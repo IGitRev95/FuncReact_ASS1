0312b96bc967c3931c3a335e7615802176a553e0
Ido <idorev@post.bgu.ac.il>
2021-11-29 15:01:22 +0200

    GetComputerAction - imp

--- src/main/java/bgu/atd/a1/sim/actions/GetComputerAction.java
+++ src/main/java/bgu/atd/a1/sim/actions/GetComputerAction.java
@@ -2,11 +2,30 @@ package bgu.atd.a1.sim.actions;
 
 import bgu.atd.a1.Action;
 import bgu.atd.a1.sim.Computer;
+import bgu.atd.a1.sim.Warehouse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class GetComputerAction extends Action<Computer> {
     @Override
     protected void start() {
-        //TODO: implement
-
+        //TODO: TEST
+        HashMap<Computer,Boolean> warehouseComputerMap = ((Warehouse)this.actorState).getComputersUsage();
+        for(Computer comp: warehouseComputerMap.keySet()){
+            if(!warehouseComputerMap.get(comp)){
+                warehouseComputerMap.put(comp,true);
+                this.complete(comp);
+                return;
+            }
+        }
+        GetComputerAction retry = new GetComputerAction();
+        List<Action<Computer>> dependencies = new ArrayList<>();
+        dependencies.add(retry);
+        then(dependencies,()->{
+            this.complete(dependencies.get(0).getResult().get());
+        });
+        this.sendMessage(retry,this.actorId,new Warehouse());
     }
 }
