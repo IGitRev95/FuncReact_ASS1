7a1442ded022523931466908e5683b57b101b750
Ido <idorev@post.bgu.ac.il>
2021-11-29 14:44:19 +0200

    Warehouse

--- src/main/java/bgu/atd/a1/sim/Warehouse.java
+++ src/main/java/bgu/atd/a1/sim/Warehouse.java
@@ -2,11 +2,25 @@ package bgu.atd.a1.sim;
 
 import bgu.atd.a1.PrivateState;
 
+import java.util.HashMap;
+
 /**
  * represents a warehouse that holds a finite amount of computers
  * and their suspended mutexes.
  * releasing and acquiring should be blocking free.
  */
 public class Warehouse extends PrivateState {
-	
+	private final HashMap<Computer,Boolean> computersUsage;
+
+    public Warehouse() {
+        this.computersUsage = new HashMap<>();
+    }
+
+    public Warehouse(HashMap<Computer, Boolean> computersUsage) {
+        this.computersUsage = computersUsage;
+    }
+
+    public HashMap<Computer, Boolean> getComputersUsage() {
+        return computersUsage;
+    }
 }
