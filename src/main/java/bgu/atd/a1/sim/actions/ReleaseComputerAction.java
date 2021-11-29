1f9241716fbe9de8a782e6a48ddd4810c5649638
Ido <idorev@post.bgu.ac.il>
2021-11-29 15:06:34 +0200

    ReleaseComputerAction - imp

--- src/main/java/bgu/atd/a1/sim/actions/ReleaseComputerAction.java
+++ src/main/java/bgu/atd/a1/sim/actions/ReleaseComputerAction.java
@@ -2,6 +2,9 @@ package bgu.atd.a1.sim.actions;
 
 import bgu.atd.a1.Action;
 import bgu.atd.a1.sim.Computer;
+import bgu.atd.a1.sim.Warehouse;
+
+import java.util.HashMap;
 
 public class ReleaseComputerAction extends Action<Boolean> {
     private final Computer comp;
@@ -12,6 +15,13 @@ public class ReleaseComputerAction extends Action<Boolean> {
 
     @Override
     protected void start() {
-        //TODO: implement
+        //TODO: TEST
+        HashMap<Computer,Boolean> warehouseComputerMap = ((Warehouse)this.actorState).getComputersUsage();
+        if(!warehouseComputerMap.get(this.comp)){
+            this.complete(false);
+        }else {
+            warehouseComputerMap.put(this.comp,false);
+            this.complete(true);
+        }
     }
 }
