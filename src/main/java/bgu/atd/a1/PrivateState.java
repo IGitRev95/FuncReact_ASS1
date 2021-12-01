78785f83a280852271070840c10b71016fdab39d
Ido <idorev@post.bgu.ac.il>
2021-12-01 15:28:56 +0200

    PrivateState - implement Serializable - added

--- src/main/java/bgu/atd/a1/PrivateState.java
+++ src/main/java/bgu/atd/a1/PrivateState.java
@@ -1,5 +1,6 @@
 package bgu.atd.a1;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,7 @@ import java.util.List;
  * it holds actions that the actor has executed so far 
  * IMPORTANT: You can not add any field to this class.
  */
-public abstract class PrivateState {
+public abstract class PrivateState implements Serializable {
 	
 	// holds the actions' name what were executed
 	private List<String> history = new ArrayList<>();
