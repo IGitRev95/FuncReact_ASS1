f8b57f01b8568be6ccf61fb5665656a66f693146
Ido <idorev@post.bgu.ac.il>
2021-12-01 15:20:41 +0200

    SerDeserializer - with ser. filr to json file

--- src/main/java/bgu/atd/a1/sim/SerDeserializer.java
+++ src/main/java/bgu/atd/a1/sim/SerDeserializer.java
@@ -1,8 +1,12 @@
 package bgu.atd.a1.sim;
 
 import bgu.atd.a1.PrivateState;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
 
@@ -15,9 +19,17 @@ public class SerDeserializer {
 
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(args[0]))){
             HashMap<String, PrivateState> map = (HashMap<String, PrivateState>) ois.readObject();
-            System.out.println(map.toString());
+//            System.out.println(map.toString());
+            objectToOutputJsonFile(map, "mapAsJson.json");
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static void objectToOutputJsonFile(Object toOutputFile, String outPutPath)  {
+        try(FileWriter outputJson = new FileWriter(new File( outPutPath ))){
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson( toOutputFile, outputJson);
+        }catch (Exception ignore){}
     }
 }
