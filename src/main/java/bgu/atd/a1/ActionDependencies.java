c17363e9e5775937300c9f82b2a2cadc6c7946a8
Ido <idorev@post.bgu.ac.il>
2021-12-06 20:39:14 +0200

    ActionDependencies - done

--- src/main/java/bgu/atd/a1/ActionDependencies.java
+++ src/main/java/bgu/atd/a1/ActionDependencies.java
@@ -2,29 +2,17 @@ package bgu.atd.a1;
 
 import java.util.*;
 
-//TODO: Build Tests
-
 public class ActionDependencies {
     /**
      * ActionDependencies - an Action dependencies data structure
      * dependenciesCollection - the actual dependencies' container
      * remainingActionCollection - <ActionName, Action> non-resolved action names container
      * allIsResolved - implies which all the dependencies are resolved
-     *
      */
 
     private final Collection<? extends Action<?>> dependenciesCollection; // used to store and quick access to the dependencies actions results
     private final LinkedList< Action<?>> remainingActionsCollection; // used to control in remaining dependencies
     private boolean allIsResolved;
-
-    /**
-     * empty constructor - creates an empty dependencies collection
-     */
-    public ActionDependencies() {
-        this.dependenciesCollection = new ArrayList<>();
-        this.remainingActionsCollection = new LinkedList<>();
-        resolvedAllCheck();
-    }
 
     /**
      * constructor - creates dependencies collection from a give collection
@@ -44,7 +32,7 @@ public class ActionDependencies {
      * the method includes the updating stage of the overall isAllResolved value.
      * @return - return bool value of all the dependencies are resolved.
      */
-    public synchronized boolean isAllResolved(){ // changed cause of flow constrains
+    public synchronized boolean isAllResolved(){ // method is synchronized causes it involves entire data structure updating
         if(!this.allIsResolved){
             List<Action<?>> remainActionNames = new ArrayList<>(this.remainingActionsCollection);
             for (Action<?> action : remainActionNames){
