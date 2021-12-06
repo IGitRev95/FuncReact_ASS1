9080aa67e6027e4ad538a6fd354ba02c8f25a52e
Ido <idorev@post.bgu.ac.il>
2021-12-06 20:15:46 +0200

    GetGradeSheetAction - done

--- src/main/java/bgu/atd/a1/sim/actions/GetGradeSheetAction.java
+++ src/main/java/bgu/atd/a1/sim/actions/GetGradeSheetAction.java
@@ -1,11 +1,9 @@
 package bgu.atd.a1.sim.actions;
 
 import bgu.atd.a1.Action;
-import bgu.atd.a1.sim.Computer;
 import bgu.atd.a1.sim.privateStates.StudentPrivateState;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class GetGradeSheetAction extends Action<HashMap<String, Integer>> {
     public GetGradeSheetAction() {
